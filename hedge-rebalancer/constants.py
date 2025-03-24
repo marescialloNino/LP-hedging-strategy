d1e1d73ae96555b4e57d562177153135f6bb3244
RichardNixon <nicola.schiavo@sudenti.unipd.it>
2025-03-24 12:43:05 +0100

    update hedgable tokens

--- hedge-rebalancer/constants.py
+++ hedge-rebalancer/constants.py
@@ -3,15 +3,33 @@
 # Hedgable tokens with addresses, symbols, and decimals
 HEDGABLE_TOKENS = {
 
+    # EVM
     "LINKUSDT": {"addresses" : ["0x53e0bca35ec356bd5dddfebbd1fc0fd03fabad39"]},
-    "ETHUSDT": {"addresses" : ["0x7ceb23fd6bc0add59e62ac25578270cff1b9f619"]},
+    "ETHUSDT": {"addresses" : ["0x7ceb23fd6bc0add59e62ac25578270cff1b9f619","0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee"]},
     "POLUSDT": {"addresses" : ["0xcccccccccccccccccccccccccccccccccccccccc"]},
     "AAVEUSDT": {"addresses" : ["0xd6df932a45c0f255f85145f286ea0b292b21c90b"]},
+     # BSC
+    "BNBUSDT" : {"addresses" : ["0xbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb"]},
+    "CAKEUSDT" : {"addresses" : ["0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82"]},
+    # Arbitrum
+    "ZROUSDT" : {"addresses" : ["0x6985884c4392d348587b19cb9eaaf157f13271cd"]},
+    "GMXUSDT" : {"addresses" : ["0xfc5a1a6eb076a2c7ad06ed22c90d7e710e35ad0a"]},
+    # Sonic
+    "SONICUSDT": {"addresses" : ["0x039e2fB66102314Ce7b64Ce5Ce3E5183bc94aD38"]},
+
+
+    # Solana
     "SOLUSDT": {"addresses" : ["So11111111111111111111111111111111111111112"]},
     # "JUPUSDT": {"addresses" : ["JUPyiwrYJFskUPiHa7hkeR8VUtAeFoSYbKedZNsDvCN"]},
     "GRIFFAINUSDT": {"addresses" : ["KENJSUYLASHUMfHyy5o4Hp2FdNqZg1AsUPhfH2kYvEP"]},
     "ARCUSDT": {"addresses" : ["61V8vBaqAGMpgDQi4JcAwo1dmBGHsyhzodcPqnEVpump"]},
+    "FARTCOINUSDT": {"addresses" : ["9BB6NFEcjBCtnNLFko2FqVQBq8HHM13kCyYcdQbgpump"]},
     "AI16ZUSDT": {"addresses" : ["HeLp6NuQkmYB4pYWo2zYs22mESHXPQYzXbB8n4V98jwC"]},
+
+
+    
+    
+
 
 }
 
